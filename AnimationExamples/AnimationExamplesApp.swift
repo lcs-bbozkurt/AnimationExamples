a9a82d387ce6b97daadf8fb83c4f990a99a3fff7
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-12-15 08:57:54 -0500

    Initial Commit

---
+++ AnimationExamples/AnimationExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExamplesApp.swift
+//  AnimationExamples
+//
+//  Created by Berk Bozkurt on 2021-12-15.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
